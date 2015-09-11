c45fceac50ad73eccaca476a6c865a016a50e476
Jeffrey Balies <jeffbailes@gmail.com>
2015-09-11 23:43:45 +1000

    Plot 4 done.

--- plot4.R
+++ plot4.R
@@ -5,9 +5,16 @@ end.Date <- as.Date("2007-02-02")
 include.obs <- (data$Date >= start.Date) & (data$Date <= end.Date)
 observations <- data[include.obs,]
 times <- strptime(paste(observations$Date, observations$Time), format="%Y-%m-%d %H:%M:%S")
-png('plot3.png', width=480, height=480)
+png('plot4.png', width=480, height=480)
+par(mfrow=c(2,2))
+plot(times,observations$Global_active_power, type='l', ylab="Global Active Power (kilowatts)", xlab="")
+
+plot(times,observations$Voltage, type='l', ylab="Voltage", xlab="datetime")
+
 plot(times, observations$Sub_metering_1, type='l',col='black', ylab="Energy sub metering")
 lines(times, observations$Sub_metering_2, col='red')
 lines(times, observations$Sub_metering_3, col='blue')
-legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c('black','red','blue'))
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c('black','red','blue'),bty='n')
+
+plot(times,observations$Global_reactive_power, type='l', ylab="Global_reactive_power", xlab="datetime")
 dev.off()
