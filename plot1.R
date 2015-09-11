4990f9438f5e4223826add4c0cda81ff2c87e9bb
Jeffrey Balies <jeffbailes@gmail.com>
2015-09-11 21:42:00 +1000

    Plot 1 Completed.

---
+++ plot1.R
@@ -0,0 +1,11 @@
+data <- read.csv("household_power_consumption.txt", sep=";",na.strings="?")
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+start.Date <- as.Date("2007-02-01")
+end.Date <- as.Date("2007-02-02")
+include.obs <- (data$Date >= start.Date) & (data$Date <= end.Date)
+#include.obs <- (data$Date == start.Date) | (data$Date == end.Date)
+observations <- data[include.obs,]
+thisData <- as.numeric(observations[,"Global_active_power"])
+png('plot1.png', width=480, height=480)
+hist(thisData,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
